b06cdb8557a9006579fdfece6241494db2bcd7be
jweboy <jweboy0630@gmail.com>
2024-12-23 02:33:13 +0800

    feat: 更新时间戳

--- src/service/task/task.service.ts
+++ src/service/task/task.service.ts
@@ -8,7 +8,7 @@ import { notifyDuckPost } from 'utils/webhook';
 export class TaskService {
   private readonly logger = new Logger(TaskService.name);
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
+  @Cron(CronExpression.EVERY_30_SECONDS)
   async collectDuckData() {
     const browser = await chromium.launch();
     const page = await browser.newPage();
